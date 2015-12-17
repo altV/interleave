69bbc6a64a4945bbccc4bb8389ef86fc8aaa926d
Leo <leonid.inbox@gmail.com>
2015-12-17 17:35:14 +0300

    works

--- lib/interleave.rb
+++ lib/interleave.rb
@@ -1,5 +1,33 @@
 require "interleave/version"
+require 'colorize'
+require 'open3'
 
 module Interleave
-  # Your code goes here...
+
+  Colors = [:yellow, :red]
+
+  def self.start
+
+    ARGV.map.with_index do |cmd, i|
+      puts "Running #{cmd}".send Colors[i]
+      Open3.popen3 cmd
+    end.
+    tap {|e| sleep 2 }.
+    each_with_index do |(r,o,e,t), i|
+      r.close
+      Thread.new do
+        loop { puts o.readline.send Colors[i] }
+      end
+    end
+
+    Thread.new do
+      loop do
+        puts Time.now.to_s.light_black
+        sleep 0.1
+      end
+    end
+
+    loop {sleep 1}
+  end
+
 end
